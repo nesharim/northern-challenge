95257947a0c46f6640bde3e05130d3f58bdc3ca9
Raphael Ribeiro <raphaelkribeiro@gmail.com>
2019-09-12 23:57:01 +0530

    Updates

---
+++ precache-manifest.159263e3a3ca8b95344f3cbabec155fd.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d1f797c77be6f548abbd1ab39e3523df",
+    "url": "/northern-challenge/index.html"
+  },
+  {
+    "revision": "510c7d616ef7a541d45c",
+    "url": "/northern-challenge/static/css/main.d534ff95.chunk.css"
+  },
+  {
+    "revision": "78047b893a4497559426",
+    "url": "/northern-challenge/static/js/2.274281ac.chunk.js"
+  },
+  {
+    "revision": "510c7d616ef7a541d45c",
+    "url": "/northern-challenge/static/js/main.33560270.chunk.js"
+  },
+  {
+    "revision": "02272567d55d31e07feb",
+    "url": "/northern-challenge/static/js/runtime~main.61202db0.js"
+  }
+]);
